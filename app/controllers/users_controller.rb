7bc4601ceea39d32d468d035c64b8d39ff8d5b24
karthykarthick <karthylock@gmail.com>
2020-10-22 22:27:14 +0530

    linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,8 +39,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
-  private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
